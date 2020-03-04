9e49fcf2a158fb6eb1d730f87ecb09701fef7e66
whitneyxlachelle <whiggins16@yahoo.com>
2020-03-03 18:14:29 -0600

    actions have been created

--- src/index.js
+++ src/index.js
@@ -5,5 +5,16 @@ import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import  { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+//createStore will take in a single reducer that represents the state of the application globally
+const store = createStore(reducer)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>,
+ rootElement
+ );
